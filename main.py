8482e7a994c28a525283fdb568c0cb161eebd214
Noa Aarts <noa@voorwaarts.nl>
2025-03-28 12:36:58 +0100

    add Pruning and Enriching

--- main.py
+++ main.py
@@ -43,23 +43,57 @@ if __name__ == "__main__":
     # TODO: parse configuration
     amount_of_chains = 300
     target_length = 1000
-    w_low = 0.33
-    w_high = 3.33
+    w_low = 1 / np.sqrt(10)
+    w_high = np.sqrt(10)
 
     # allow for all three coordinates up to the max length for each chain
-    chains = np.zeros((amount_of_chains, target_length, 2))
+    chains = np.zeros((amount_of_chains, target_length + 1, 2))
     # keeps track of whether to keep growing a specific chain or not and the timesteps
-    alive = np.tile(True, (amount_of_chains, target_length))
+    alive = np.tile(True, (amount_of_chains, target_length + 1))
     # weight for each sub-length L for each of the chains
     # uses the long double datatype 'g' (probably an 80 bit float) to allow for the big numbers that may appear
-    weights = np.zeros((amount_of_chains, target_length), dtype="g")
+    weights = np.zeros((amount_of_chains, target_length + 1), dtype="g")
     weights[:, 0] = 1
 
     # TODO: iterate on the polymers using PERM instead of just Rosenbluth
-    for step in range(target_length - 1):
+    for step in range(target_length):
         for chain in range(amount_of_chains):
             do_step(chains[chain, :, :], weights[chain, :], alive[chain, :], step)
 
+        # we use step+1 to get the L'
+        mean_weight = np.mean(weights[alive[:, step + 1], step + 1])
+        to_add = []
+        pruned = 0
+        for chain in range(amount_of_chains):
+            if not alive[chain, step + 1]:
+                continue
+            # Pruning
+            if weights[chain, step + 1] < w_low * mean_weight:
+                if random() < 0.5:
+                    pruned += 1
+                    # don't grow this chain anymore
+                    alive[chain, step + 1] = False
+                    # discard the weight at length L'
+                    weights[chain, step + 1] = 0
+                else:
+                    weights[chain, step + 1] *= 2
+            # Enrichment
+            elif weights[chain, step + 1] > w_high * mean_weight:
+                # TODO: double and half weight
+                weights[chain, step + 1] /= 2
+                to_add.append(chain)
+        chains = np.concatenate(
+            [chains] + [np.expand_dims(chains[chain, :, :], 0) for chain in to_add],
+            axis=0,
+        )
+        weights = np.concatenate(
+            [weights] + [np.expand_dims(weights[chain, :], 0) for chain in to_add],
+            axis=0,
+        )
+        alive = np.concatenate(
+            [alive] + [np.expand_dims(alive[chain, :], 0) for chain in to_add], axis=0
+        )
+        amount_of_chains = chains.shape[0]
     # TODO: calculate end to end distances
 
     # TODO: calculate gyrations
