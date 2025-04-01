f4168ce8b33e50dfa9f822d719e5080b6a1ea98b
Noa Aarts <noa@voorwaarts.nl>
2025-04-01 22:00:49 +0200

    move polymer growing to function

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 from random import choice, random
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm
+import logging
+from tqdm import trange
+from tqdm.contrib.logging import logging_redirect_tqdm
+
+LOG = logging.getLogger(__name__)
 
 
 def get_allowed_sides_2d(chain, step):
@@ -39,6 +43,60 @@ def do_step(chain, weight, alive, step, next_sites_function):
         alive[step + 1 :] = False
 
 
+def grow_polymers(chains, alive, weights, amount_of_chains, next_sides_function):
+    with logging_redirect_tqdm():
+        max_step = 1
+        for step in trange(target_length):
+            for chain in range(amount_of_chains):
+                do_step(
+                    chains[chain, :, :],
+                    weights[chain, :],
+                    alive[chain, :],
+                    step,
+                    next_sides_function,
+                )
+
+            # we use step+1 to get the L'
+            if (alive[:, step + 1] == False).all():
+                LOG.warning(f"All chains died by step {step + 1}, skipping other steps")
+                break
+            mean_weight = np.mean(weights[alive[:, step + 1], step + 1])
+            to_add = []
+            pruned = 0
+            for chain in range(amount_of_chains):
+                if not alive[chain, step + 1]:
+                    continue
+                # Pruning
+                if weights[chain, step + 1] < w_low * mean_weight:
+                    if random() < 0.5:
+                        pruned += 1
+                        # don't grow this chain anymore
+                        alive[chain, step + 1] = False
+                        # discard the weight at length L'
+                        weights[chain, step + 1] = 0
+                    else:
+                        weights[chain, step + 1] *= 2
+                # Enrichment
+                elif weights[chain, step + 1] > w_high * mean_weight:
+                    # TODO: double and half weight
+                    weights[chain, step + 1] /= 2
+                    to_add.append(chain)
+            chains = np.concatenate(
+                [chains] + [chains[np.newaxis, chain, :, :] for chain in to_add],
+                axis=0,
+            )
+            weights = np.concatenate(
+                [weights] + [weights[np.newaxis, chain, :] for chain in to_add],
+                axis=0,
+            )
+            alive = np.concatenate(
+                [alive] + [alive[np.newaxis, chain, :] for chain in to_add], axis=0
+            )
+            amount_of_chains = chains.shape[0]
+            max_step += 1
+    return max_step, chains.shape[0], chains, alive, weights
+
+
 if __name__ == "__main__":
     # TODO: parse configuration
     amount_of_chains = 300
@@ -56,56 +114,10 @@ if __name__ == "__main__":
     # uses the long double datatype 'g' (probably an 80 bit float) to allow for the big numbers that may appear
     weights = np.zeros((amount_of_chains, target_length + 1), dtype="g")
     weights[:, 0] = 1
-    max_step = 1
 
-    for step in tqdm(range(target_length)):
-        for chain in range(amount_of_chains):
-            do_step(
-                chains[chain, :, :],
-                weights[chain, :],
-                alive[chain, :],
-                step,
-                next_sides_function,
-            )
-
-        # we use step+1 to get the L'
-        if (alive[:, step + 1] == False).all():
-            print(f"All chains died by step {step + 1}, skipping other steps")
-            break
-        mean_weight = np.mean(weights[alive[:, step + 1], step + 1])
-        to_add = []
-        pruned = 0
-        for chain in range(amount_of_chains):
-            if not alive[chain, step + 1]:
-                continue
-            # Pruning
-            if weights[chain, step + 1] < w_low * mean_weight:
-                if random() < 0.5:
-                    pruned += 1
-                    # don't grow this chain anymore
-                    alive[chain, step + 1] = False
-                    # discard the weight at length L'
-                    weights[chain, step + 1] = 0
-                else:
-                    weights[chain, step + 1] *= 2
-            # Enrichment
-            elif weights[chain, step + 1] > w_high * mean_weight:
-                # TODO: double and half weight
-                weights[chain, step + 1] /= 2
-                to_add.append(chain)
-        chains = np.concatenate(
-            [chains] + [chains[np.newaxis, chain, :, :] for chain in to_add],
-            axis=0,
-        )
-        weights = np.concatenate(
-            [weights] + [weights[np.newaxis, chain, :] for chain in to_add],
-            axis=0,
-        )
-        alive = np.concatenate(
-            [alive] + [alive[np.newaxis, chain, :] for chain in to_add], axis=0
-        )
-        amount_of_chains = chains.shape[0]
-        max_step += 1
+    max_step, amount_of_chains, chains, alive, weights = grow_polymers(
+        chains, alive, weights, amount_of_chains, next_sides_function
+    )
 
     print(max_step, amount_of_chains)
     end_to_ends = np.zeros((amount_of_chains, max_step))
